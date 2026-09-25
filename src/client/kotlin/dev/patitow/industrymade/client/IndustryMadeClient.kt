7da5d933fa08ccb47d68ded0fe0e2c8945bfb932
patitow <msoliver2002@gmail.com>
2026-09-24 21:25:38 -0300

    feat: initialize project structure for Industry Made Fabric mod

---
+++ src/client/kotlin/dev/patitow/industrymade/client/IndustryMadeClient.kt
@@ -0,0 +1,9 @@
+package dev.patitow.industrymade.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object IndustryMadeClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
